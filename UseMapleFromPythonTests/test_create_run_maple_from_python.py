1564bf056934d792c3747cb10f40e06bd6b10e7b
Tereso del Rio <teresodra@gmail.com>
2023-04-28 09:47:09 +0100

    Adding a simple test

--- UseMapleFromPythonTests/test_create_run_maple_from_python.py
+++ UseMapleFromPythonTests/test_create_run_maple_from_python.py
@@ -5,8 +5,6 @@ sys.path.append('./')
 id = "oyl"
 timeout = 10
 initial_directory = os.path.join(os.path.dirname(__file__),'..','..')
-polynomials = [[[1, 0, 1]], [[0, 1, 1]], [[0, 0, -213], [0, 1, 655]]]
-variable = 1
 auxiliar_file = os.path.join(initial_directory, '03CADVariableOrdering',
                              "Auxiliar", "maple_from_python_file_proj"
                              + id + ".mpl")
@@ -14,9 +12,25 @@ output_file = os.path.join(initial_directory, '03CADVariableOrdering',
                            "Auxiliar", "maple_from_python_output"+id+".txt")
 
 
-def test_create_run_maple_from_python():
+def test_create_run_maple_from_python_gcd():
     """This is a shitty test but it is best than anything."""
-    from UseMapleFromPython.use_maple_from_python import create_run_maple_from_python
+    from UseMapleFromPython import create_run_maple_from_python
+    aux = create_run_maple_from_python(
+        file_location=auxiliar_file,  # equvalent to
+        initializations=[("a", 6), ("b", -8)],  # a:=6:b=-8:
+        functions_to_call=[("gcd",
+                           ["a", "b"],
+                           "result")],  # result := gcd(a,b)
+        output_files=[(output_file, "result")], timelimit=timeout) # (python) aux[2] = result 
+
+    assert aux[2] == 2, ("The return number is not the gcd")
+
+
+def test_create_run_maple_from_python_projection():
+    """This is a shitty test but it is best than anything."""
+    from UseMapleFromPython import create_run_maple_from_python
+    polynomials = [[[1, 0, 1]], [[0, 1, 1]], [[0, 0, -213], [0, 1, 655]]]
+    variable = 1
     aux = create_run_maple_from_python(
         file_location=auxiliar_file,
         libnames_needed=[os.path.join(initial_directory,
@@ -30,3 +44,6 @@ def test_create_run_maple_from_python():
 
     assert aux[2] == [[[1, 1]], [[0, -213], [1, 655]]], ("The return"
            "polynomials aren't the projection")
+
+
+test_create_run_maple_from_python_gcd()
